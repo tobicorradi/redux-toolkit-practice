1892f9845410360253ef0b06edd2cba6b1f615e0
tobicorradi <corraditobias@gmail.com>
2022-04-10 16:41:47 -0300

    Post Added action created inside slice

--- src/features/posts/postsSlice.jsx
+++ src/features/posts/postsSlice.jsx
@@ -9,10 +9,14 @@ export const postsSlice = createSlice({
     name: 'posts',
     initialState,
     reducers: {
-
+        postAdded(state, action){
+            state.push(action.payload)
+        }
     }
 })
 
 export const selectAllPosts = (state) => state.posts
+
+export const {postAdded} = postsSlice.actions
 
 export default postsSlice.reducer
